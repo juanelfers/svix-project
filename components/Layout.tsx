9d6aea59391270248e086bfc5571b177f5b18184
Juan Elfers <juan.elfers@gmail.com>
2023-06-07 14:25:24 -0300

    Fixing FOUC with Chakra styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,16 @@
 import React, { ReactNode } from 'react'
 import { Container } from '@chakra-ui/react'
-
+import Document, { DocumentContext, Html, Main, NextScript } from "next/document";
 import Head from 'next/head'
+
+import emotionCache from "../lib/emotion-cache";
+import createEmotionServer from "@emotion/server/create-instance";
 
 import Providers from './Providers'
 import Header from './Header'
 import Footer from './Footer'
+
+const { extractCritical } = createEmotionServer(emotionCache);
 
 type Props = {
   children?: ReactNode
@@ -27,4 +32,23 @@ const Layout = ({ children, title = 'Svix' }: Props) => (
   </Providers>
 )
 
-export default Layout
+Layout.getInitialProps = async (ctx: DocumentContext) => {
+  const initialProps = await Document.getInitialProps(ctx);
+  console.log({ initialProps })
+  const styles = extractCritical(initialProps.html);
+  return {
+    ...initialProps,
+    styles: [
+      initialProps.styles,
+      <style
+        key="emotion-css"
+        dangerouslySetInnerHTML={{ __html: styles.css }}
+        data-emotion-css={styles.ids.join(" ")}
+      />,
+    ],
+  };
+}
+
+
+export default Layout;
+
