c31175fa49455900c6c101618df2ba96977a00d7
Juan Elfers <juan.elfers@gmail.com>
2023-06-07 16:16:30 -0300

    Added basic navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,6 @@
 import { Box } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { Link } from '@chakra-ui/react'
 
 import Image from 'next/image';
 import SvixLogo from '../assets/img/svix-logo.svg'
@@ -7,6 +9,9 @@ export default function Header() {
     return (
         <Box as="header">
             <Image src={SvixLogo} width={500} alt="Svix Logo" />
+            
+            <Link as={NextLink} href='/'>Home</Link>
+            <Link as={NextLink} href='/new-event'>New event</Link>
         </Box>
     )
 }
